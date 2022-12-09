0b4ad9c41a574e81c56af7a06310da8fd0040869
dreamflyforever <1150660744@qq.com>
2022-12-09 12:37:54 +0800

    classify the directory for mtcc crop picture
    Signed-off-by: dreamflyforever <1150660744@qq.com>

--- predict.py
+++ predict.py
@@ -6,27 +6,35 @@ import os
 import glob
 
 img_embedding_list = []
+
+# get embedding
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
-# If required, create a face detection pipeline using MTCNN:
+
+# get face detect 
 mtcnn = MTCNN(image_size= 360, margin = 0)
-WSI_MASK_PATH = 'photo_dataset'#存放图片的文件夹路径
+
+# face database
+WSI_MASK_PATH = 'photo_dataset'
 paths = glob.glob(os.path.join(WSI_MASK_PATH, '*.png'))
 paths.sort()
 tmp = []
 for i in paths:
     img2 = Image.open(i)
     img2 = img2.convert('RGB')
-    img_cropped2 = mtcnn(img2, save_path="{}.jpg".format(i))
-
+    file_name = os.path.basename('{}'.format(i))
+    img_cropped2 = mtcnn(img2, save_path="mtcnn_crop/{}".format(file_name))
     img_embedding2 = resnet(img_cropped2.unsqueeze(0))
     name = os.path.splitext(i)[0]
     img_embedding_list.append([img_embedding2, name])
 
-print("{}".format(img_embedding_list[0][1]))
-# Create an inception resnet (in eval mode):
+print('=============> database face name <=================')
+for index in range(len(img_embedding_list)):
+	print("{}".format(img_embedding_list[index][1]))
+print('====================================================')
 
+# check the face recognize
 img = Image.open("photo_dataset/jim.png")
-print(img.mode)
+#print(img.mode)
 img = img.convert('RGB')
 # Get cropped and prewhitened image tensor
 img_cropped = mtcnn(img, save_path="photo_dataset/360.jpg")
